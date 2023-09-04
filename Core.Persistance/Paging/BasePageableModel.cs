6f7f48d84700ad2df5e68ffd2b595b23bad0f544
Resul Pala <resulpala@hotmail.com.tr>
2023-09-04 23:22:08 +0300

    Core.Application layer created

---
+++ Core.Persistance/Paging/BasePageableModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Paging;
+
+public abstract class BasePageableModel
+{
+    public int Size { get; set; } //sayfada kaç data var
+    public int Index { get; set; } //hangi sayfadayız
+    public int Count { get; set; } //toplam kayıt sayısı
+    public int Pages { get; set; } //toplam kaç sayfa var 
+    public bool HasPrevious { get; set; }
+    public bool HasNext { get; set; }
+    
+}
